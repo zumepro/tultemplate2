dcc81b36b818f0ddb21b8add4db8a2bd0f422c3d
Ondřej Mekina <mekina@duck.com>
2026-02-02 22:44:17 +0100

    fix domain in minimal.mk

--- minimal.mk
+++ minimal.mk
@@ -8,7 +8,7 @@ $(TARGET).pdf: $(TARGET).typ template
 	typst compile --font-path template/fonts $< $@
 
 tultemplate2_minimal.zip:
-	wget https://tulsablona.zumepro.cz/tultemplate2_minimal.zip
+	wget https://typst.tul.cz/tultemplate2_minimal.zip
 
 .PRECIOUS: template
 template: tultemplate2_minimal.zip
